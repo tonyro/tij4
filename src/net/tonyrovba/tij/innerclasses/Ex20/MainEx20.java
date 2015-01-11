c7a554d68a030c8d4695e217f83402ade1f3bcb9
Anton Rovba <tony.rovba@gmail.com>
2015-01-11 11:14:18 +0200

    [Innerclasses - Ex20] Added Vet implementation

--- src/net/tonyrovba/tij/innerclasses/Ex20/MainEx20.java
+++ src/net/tonyrovba/tij/innerclasses/Ex20/MainEx20.java
@@ -5,8 +5,11 @@ package net.tonyrovba.tij.innerclasses.Ex20;
  */
 public class MainEx20 {
     public static void main(String[] args) {
-        Doctor.BaseDoctor doc = new Doctor.BaseDoctor();
 
+        Doctor.BaseDoctor doc = new Doctor.BaseDoctor();
         doc.greats();
+
+        Vet vet = new Vet();
+        vet.greats();
     }
 }
---
+++ src/net/tonyrovba/tij/innerclasses/Ex20/Vet.java
@@ -0,0 +1,10 @@
+package net.tonyrovba.tij.innerclasses.Ex20;
+
+/**
+ * Created by Anton on 11.01.2015.
+ */
+public class Vet implements Doctor {
+    public void greats() {
+        System.out.println("Greatings, animal!");
+    }
+}
