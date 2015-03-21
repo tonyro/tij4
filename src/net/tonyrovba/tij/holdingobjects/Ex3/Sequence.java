278145ce00c9c91b32261c86d414f1ae64002f8c
tonyro <tony.rovba@gmail.com>
2015-03-21 11:35:28 +0200

    [Holdingobjects.Ex3] Implemented

---
+++ src/net/tonyrovba/tij/holdingobjects/Ex3/Sequence.java
@@ -0,0 +1,55 @@
+package net.tonyrovba.tij.holdingobjects.Ex3;
+
+import java.util.ArrayList;
+
+/**
+ * @author tonyro
+ */
+interface Selector {
+    boolean end();
+    Object current();
+    void next();
+}
+
+public class Sequence {
+    private ArrayList<Object> items;
+
+    public Sequence() { items = new ArrayList<>(); }
+
+    public void add(Object x) {
+        items.add(x);
+    }
+
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+        public boolean end() { return i == items.size(); }
+        public Object current() { return items.get(i); }
+        public void next() { if(i < items.size()) i++; }
+    }
+
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence();
+        for(int i = 0; i < 10; i++)
+            sequence.add(Integer.toString(i));
+        Selector selector = sequence.selector();
+        while(!selector.end()) {
+            System.out.print(selector.current() + " ");
+            selector.next();
+        }
+
+        System.out.println();
+
+        Sequence sequence1 = new Sequence();
+        for(int i = 0; i < 30; i++)
+            sequence1.add(Integer.toString(i));
+        Selector selector1 = sequence.selector();
+        while(!selector1.end()) {
+            System.out.print(selector1.current() + " ");
+            selector1.next();
+        }
+    }
+}
