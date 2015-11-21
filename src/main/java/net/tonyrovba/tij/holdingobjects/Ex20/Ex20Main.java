efb875efd2ba1d72e82882901d5b2dcf3664ecb8
tonyro <tony.rovba@gmail.com>
2015-11-21 21:53:06 +0200

    [HoldingObjects.Ex20] Implemented

--- src/main/java/net/tonyrovba/tij/holdingobjects/Ex20/Ex20Main.java
+++ src/main/java/net/tonyrovba/tij/holdingobjects/Ex20/Ex20Main.java
@@ -1,4 +1,4 @@
-package net.tonyrovba.tij.holdingobjects.Ex16;
+package net.tonyrovba.tij.holdingobjects.Ex20;
 
 import java.io.File;
 import java.security.CodeSource;
@@ -6,16 +6,27 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 /**
- * Created by arovba on 11/8/15.
+ * Created by arovba on 11/10/15.
  *
- * (5) Create a Set of the vowels. Working from UniqueWords.Java, count and display the number of vowels
- * in each input word, and also display the total number of vowels in the input file.
+ * (3) Modify Exercise 16 from this package so that you keep a count of the occurrence of each vowel.
  *
  */
 
-public class Ex16Main {
+public class Ex20Main {
+    static Map<Character,Integer> eachVowelCount = new HashMap<>();
+
+    static {
+        eachVowelCount.put('a', 0);
+        eachVowelCount.put('e', 0);
+        eachVowelCount.put('i', 0);
+        eachVowelCount.put('o', 0);
+        eachVowelCount.put('u', 0);
+        eachVowelCount.put('y', 0);
+    }
+
+
     public static void main(String[] args) {
-        final char[] VOWELS_STRING = "AEIOUYaeiouy".toCharArray();
+        final char[] VOWELS_STRING = "aeiouy".toCharArray();
         final String FILE_NAME = "UniqueWords.txt";
         String filePath;
         ArrayList<String> words = new ArrayList<>();
@@ -25,7 +36,7 @@ public class Ex16Main {
         for (char ch: VOWELS_STRING)
             vowels.add(ch);
 
-        CodeSource src = Ex16Main.class.getProtectionDomain().getCodeSource();
+        CodeSource src = Ex20Main.class.getProtectionDomain().getCodeSource();
         try {
             filePath = src.getLocation().toURI().getPath() + FILE_NAME;
             File file = new File(filePath);
@@ -40,15 +51,19 @@ public class Ex16Main {
 
         for (String word: words){
             int wordVowelsCount = 0;
-
-            for (int i = 0; i < word.length(); i++)
-                if (vowels.contains(word.charAt(i))){
+            String normilizedWord = word.toLowerCase();
+            for (int i = 0; i < normilizedWord.length(); i++) {
+                Character character = normilizedWord.charAt(i);
+                if (vowels.contains(character)) {
                     wordVowelsCount++;
                     overallVowelsCount++;
+                    eachVowelCount.put(character, eachVowelCount.get(character) + 1);
                 }
+            }
             System.out.println("Word: [ " + word + " ] contains [ " + wordVowelsCount + " ] vowel(s).");
         }
 
         System.out.println("All words contain [ " + overallVowelsCount + " ] vowel(s).");
+        System.out.println(eachVowelCount);
     }
 }
